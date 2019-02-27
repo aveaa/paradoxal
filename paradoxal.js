569d34d5fd64bc8464cfba8add72ed5391975d4a
Freezer <36858947+freezerXdev@users.noreply.github.com>
2019-02-27 21:41:45 +0200

    Update paradoxal.js

--- paradoxal.js
+++ paradoxal.js
@@ -27,6 +27,20 @@ client.on('ready', () => {
  }; setInterval(randomStatus, 10000)
 });
 
+// Ивент при заходе человека на сервер ➕
+client.on('guildMemberAdd',(member) => member.guild.channels.get('548599362867953758').send(`◉ ${member}`, 
+       new Discord.RichEmbed()
+   .setThumbnail(member.user.avatarURL)                                                                                         
+   .setTitle("Приветствуем вас на нашем сервере!")
+                .setDescription(`Приветствуем на Paradoxal. Ты стал ${member.guild.memberCount} участником нашего сервера!
+Прочитай <#547759714574794762> и начинай общаться!`)
+                .setFooter("Спасибо за то, что присоединились к нам!")
+                .setColor(c)
+              ).then( msg => {
+    msg.react("🎉")
+    } )
+)
+
     client.login(process.env.TOKEN);
 
     client.on('message', message => {
